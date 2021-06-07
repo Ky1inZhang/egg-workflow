c2af7f4245cbad9f9b835c014aa94d0da3ef9445
Ky1inZhang <a1179112978@gmail.com>
2021-06-07 14:16:45 +0800

    Update config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -16,8 +16,8 @@ module.exports = appInfo => {
   config.cluster = {
     listen: {
       path: '',
-      port: 80,
-      //port: process.env.PORT,
+      //port: 80,
+      port: process.env.PORT,
       // hostname: '192.168.43.16',
       //hostname: '127.0.0.1',
     }
